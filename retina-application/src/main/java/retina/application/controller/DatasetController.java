5ee0bceb6a73af9e4855a4660de261f8bb96cfa2
Serj <Serj@DESKTOP-S50IUR9>
2020-10-07 10:17:31 +0300

    controller

---
+++ retina-application/src/main/java/retina/application/controller/DatasetController.java
@@ -0,0 +1,10 @@
+package retina.application.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dataset")
+public class DatasetController {
+
+}
